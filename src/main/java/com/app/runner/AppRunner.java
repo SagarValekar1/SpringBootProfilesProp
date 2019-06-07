53b42e04b448b54898e6cb7bd1e4416d9550ecee
valekar <valekar@sagar>
2019-06-07 15:15:54 +0530

    All file added

---
+++ src/main/java/com/app/runner/AppRunner.java
@@ -0,0 +1,24 @@
+package com.app.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class AppRunner implements CommandLineRunner {
+	@Value("${my.data.code}")
+	private String code;
+	@Value("${my.data.code}")
+	private String mode;
+	
+@Override
+public void run(String... args) throws Exception {
+	System.out.println(this);
+	
+}
+
+@Override
+public String toString() {
+	return "AppRunner [code=" + code + ", mode=" + mode + "]";
+}
+
+}
